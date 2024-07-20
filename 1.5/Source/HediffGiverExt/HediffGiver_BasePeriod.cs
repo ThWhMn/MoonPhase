010592f6f9723ecb1048822386905a6bba571497
ThWhMn <1692657550@qq.com>
2024-07-20 16:48:15 +0800

    修复只有一个pawn获得了hediff
    简单提取HediffGiver_BasePeriod基类
    
    (cherry picked from commit 961141b78c8792e70f1ee23794ad8b96f3fc10b2)

---
+++ 1.5/Source/HediffGiverExt/HediffGiver_BasePeriod.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MoonPhase.Wolfein.HediffGiverExt;
+public class HediffGiver_BasePeriod : HediffGiver
+{
+    protected int startTimeHour;
+    protected int endTimeHour { get => startTimeHour + periodHour; }
+    protected int periodHour;
+    protected bool sendMsg = false;
+
+    protected Dictionary<Pawn, Dictionary<HediffDef, int>> pawn2hediff2endtick = new();
+
+    virtual protected bool DuringPeriod(Pawn pawn)
+    {
+        Log.Error("DuringPeriod not override");
+        return false;
+    }
+
+    protected int TicksLeft(Pawn pawn)
+    {
+        var now = GenLocalDate.HourInteger(pawn);
+        return (endTimeHour - now) * GenDate.TicksPerHour;
+    }
+
+    public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+    {
+        if (DuringPeriod(pawn))
+        {
+            if (!Utils.PawnHasHediff(pawn, hediff))
+            {
+                // Give pawn hediff
+                if (hediff.comps.Find(x => x.GetType() == typeof(HediffCompProperties_Disappears)) is not HediffCompProperties_Disappears thatComp)
+                {
+                    Log.Error("HediffCompProperties_Disappears not found");
+                    return;
+                }
+                var ticksLeft = TicksLeft(pawn);
+                thatComp.disappearsAfterTicks = new IntRange
+                {
+                    min = ticksLeft,
+                    max = ticksLeft + GenTicks.TickRareInterval
+                };
+                pawn2hediff2endtick[pawn] = new()
+                {
+                    [hediff] = GenTicks.TicksAbs + ticksLeft
+                };
+                var ret = TryApply(pawn);
+                if (ret && sendMsg)
+                    SendLetter(pawn, cause);
+            }
+        }
+    }
+}
---
+++ 1.5/Source/HediffGiverExt/Utils.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace MoonPhase.Wolfein.HediffGiverExt;
+internal static class Utils
+{
+
+    public static bool PawnHasHediff(Pawn pawn, HediffDef def)
+    {
+        HediffSet hediffSet = pawn.health.hediffSet;
+        foreach (var hediff in hediffSet.hediffs)
+        {
+            if (hediff.def.defName == def.defName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
