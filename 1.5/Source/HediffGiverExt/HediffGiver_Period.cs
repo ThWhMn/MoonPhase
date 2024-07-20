961141b78c8792e70f1ee23794ad8b96f3fc10b2
ThWhMn <1692657550@qq.com>
2024-07-20 16:35:15 +0800

    修复只有一个pawn获得了hediff
    简单提取HediffGiver_BasePeriod基类

--- 1.5/Source/HediffGiverExt/HediffGiver_Period.cs
+++ 1.5/Source/HediffGiverExt/HediffGiver_Period.cs
@@ -1,55 +1,15 @@
 ﻿using RimWorld;
-using System.Collections.Generic;
 using Verse;
 
 namespace MoonPhase.HediffGiverExt;
 
-public class HediffGiver_Period : HediffGiver
+public class HediffGiver_Period : HediffGiver_BasePeriod
 {
-    private int startTimeHour;
-    private int endTimeHour { get => startTimeHour + periodHour; }
-    private int periodHour;
-    private bool sendMsg = false;
-
-    private Dictionary<HediffDef, int> hediff2endtick = new();
-
-    private bool DuringPeriod(Pawn pawn)
+    protected override bool DuringPeriod(Pawn pawn)
     {
         var now = GenLocalDate.HourInteger(pawn);
         return startTimeHour <= now && now < endTimeHour;
         //跨天时，例如23-次日6点，该函数也只在23点返回true并触发带时限的hediff
         //所以无需对跨天数做特殊处理
-    }
-
-    private int TicksLeft(Pawn pawn)
-    {
-        var now = GenLocalDate.HourInteger(pawn);
-        return (endTimeHour - now) * GenDate.TicksPerHour;
-    }
-
-    public override void OnIntervalPassed(Pawn pawn, Hediff cause)
-    {
-        if (DuringPeriod(pawn))
-        {
-            if (!hediff2endtick.ContainsKey(hediff) ||
-                GenTicks.TicksAbs > hediff2endtick[hediff])
-            {
-                if (hediff.comps.Find(x => x.GetType() == typeof(HediffCompProperties_Disappears)) is not HediffCompProperties_Disappears thatComp)
-                {
-                    Log.Error("HediffCompProperties_Disappears not found");
-                    return;
-                }
-                var ticksLeft = TicksLeft(pawn);
-                thatComp.disappearsAfterTicks = new IntRange
-                {
-                    min = ticksLeft,
-                    max = ticksLeft + GenTicks.TickRareInterval
-                };
-                hediff2endtick[hediff] = GenTicks.TicksAbs + ticksLeft;
-                var ret = TryApply(pawn);
-                if (ret && sendMsg)
-                    SendLetter(pawn, cause);
-            }
-        }
     }
 }
--- 1.5/Source/HediffGiverExt/HediffGiver_PeriodAtPhase.cs
+++ 1.5/Source/HediffGiverExt/HediffGiver_PeriodAtPhase.cs
@@ -4,17 +4,11 @@ using Verse;
 
 namespace MoonPhase.HediffGiverExt;
 
-public class HediffGiver_PeriodAtPhase : HediffGiver
+public class HediffGiver_PeriodAtPhase : HediffGiver_BasePeriod
 {
     private List<string> phases;
-    private int startTimeHour;
-    private int endTimeHour { get => startTimeHour + periodHour; }
-    private int periodHour;
-    private bool sendMsg = false;
 
-    private Dictionary<HediffDef, int> hediff2endtick = new();
-
-    private bool DuringPeriod(Pawn pawn)
+    protected override bool DuringPeriod(Pawn pawn)
     {
         if (!Main.InPhaseList(phases))
             return false;
@@ -22,37 +16,5 @@ public class HediffGiver_PeriodAtPhase : HediffGiver
         return startTimeHour <= now && now < endTimeHour;
         //跨天时，例如23-次日6点，该函数也只在23点返回true并触发带时限的hediff
         //所以无需对跨天数做特殊处理
-    }
-
-    private int TicksLeft(Pawn pawn)
-    {
-        var now = GenLocalDate.HourInteger(pawn);
-        return (endTimeHour - now) * GenDate.TicksPerHour;
-    }
-
-    public override void OnIntervalPassed(Pawn pawn, Hediff cause)
-    {
-        if (DuringPeriod(pawn))
-        {
-            if (!hediff2endtick.ContainsKey(hediff) ||
-                GenTicks.TicksAbs > hediff2endtick[hediff])
-            {
-                if (hediff.comps.Find(x => x.GetType() == typeof(HediffCompProperties_Disappears)) is not HediffCompProperties_Disappears thatComp)
-                {
-                    Log.Error("HediffCompProperties_Disappears not found");
-                    return;
-                }
-                var ticksLeft = TicksLeft(pawn);
-                thatComp.disappearsAfterTicks = new IntRange
-                {
-                    min = ticksLeft,
-                    max = ticksLeft + GenTicks.TickRareInterval
-                };
-                hediff2endtick[hediff] = GenTicks.TicksAbs + ticksLeft;
-                var ret = TryApply(pawn);
-                if (ret && sendMsg)
-                    SendLetter(pawn, cause);
-            }
-        }
     }
 }
